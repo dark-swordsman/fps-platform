51dfaf3a279fb9cfcbe745e2384d5bb809172644
Kyle Barr <kylebarr54321@gmail.com>
2024-05-01 12:07:00 -0600

    create basic menu (+ new skybox)

---
+++ Assets/my assets/MenuController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MenuUI;
+    private bool menuIsOpen = false;
+
+    // Unity methods
+
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // custom methods
+
+    public bool GetMenuState() {
+        return menuIsOpen;
+    }
+
+    private void CursorLock(bool state) {
+        if (state) {
+            MenuUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        } else {
+            Cursor.lockState = CursorLockMode.Locked;
+            MenuUI.SetActive(false);
+        }
+    }
+
+    // hooks
+
+    public void ShowMenu() {
+        menuIsOpen = !menuIsOpen;
+        Debug.Log(menuIsOpen);
+        CursorLock(menuIsOpen);
+    }
+}
